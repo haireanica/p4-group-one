30de07e6e2bde6339731a9c71d43fd4109148ea5
Steven Petocz <stevenpetocz@gmail.com>
2026-09-16 15:37:43 -0400

    Initial iOS Project

---
+++ LendITiOS/LendITiOS/LendITiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LendITiOSApp.swift
+//  LendITiOS
+//
+//  Created by Steven Petocz on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct LendITiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
